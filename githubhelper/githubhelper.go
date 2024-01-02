a9e9d22fda104df4f86169cf896ba1acfe545705
yuta-2001 <yu0210ta.sug@gmail.com>
2024-01-02 14:36:27 +0700

    fix

--- githubhelper/githubhelper.go
+++ githubhelper/githubhelper.go
@@ -36,26 +36,20 @@ type Response struct {
 
 func GetContributesCount() int {
 	accessToken := os.Getenv("GITHUB_TOKEN")
-	// JSTのロケーションを取得
-	loc, err := time.LoadLocation("Asia/Tokyo")
+	timezone := os.Getenv("TIMEZONE")
+
+	loc, err := time.LoadLocation(timezone)
 	if err != nil {
 		panic(err)
 	}
 
-	// 現在の日付と時刻をJSTで取得
 	nowJST := time.Now().In(loc)
-
-	// 現在の日付の0時0分0秒をJSTで取得
 	startOfTodayJST := time.Date(nowJST.Year(), nowJST.Month(), nowJST.Day(), 0, 0, 0, 0, loc)
-
-	// 現在の日付の23時59分59秒をJSTで取得
 	endOfTodayJST := startOfTodayJST.Add(23*time.Hour + 59*time.Minute + 59*time.Second)
 
-	// JSTをUTCに変換
 	startOfTodayUTC := startOfTodayJST.UTC()
 	endOfTodayUTC := endOfTodayJST.UTC()
 
-	// UTCの時間をISO8601フォーマットの文字列に変換
 	startOfTodayStr := startOfTodayUTC.Format(time.RFC3339)
 	endOfTodayStr := endOfTodayUTC.Format(time.RFC3339)
 
@@ -77,26 +71,22 @@ func GetContributesCount() int {
 			}
 		}
 	`
-	// クエリの変数
 	variables := map[string]interface{}{
 		"userName": os.Getenv("GITHUB_USER"),
-		"from":     startOfTodayStr, // JSTの2024年1月3日の開始をUTCで表現
-		"to":       endOfTodayStr, // JSTの2024年1月3日の終了をUTCで表現
+		"from":     startOfTodayStr,
+		"to":       endOfTodayStr,
 	}
 
-	// GraphQLリクエストオブジェクトを作成
 	graphqlRequest := GraphQLRequest{
 		Query:     query,
 		Variables: variables,
 	}
 
-	// GraphQLリクエストオブジェクトをJSONに変換
 	requestBody, err := json.Marshal(graphqlRequest)
     if err != nil {
         panic(err)
     }
 
-    // POSTリクエストを作成
     req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
     if err != nil {
         panic(err)
@@ -105,7 +95,6 @@ func GetContributesCount() int {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer " + accessToken)
 
-    // HTTPクライアントを作成し、リクエストを実行
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
@@ -113,7 +102,6 @@ func GetContributesCount() int {
     }
     defer resp.Body.Close()
 
-	// レスポンスを読み込み
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
@@ -124,7 +112,6 @@ func GetContributesCount() int {
 		panic(err)
 	}
 
-	// レスポンスからコントリビュート数を取得
 	var contributesCount int
 	for _, week := range response.Data.User.ContributionCollection.ContributionCalendar.Weeks {
 		for _, day := range week.ContributionDays {
