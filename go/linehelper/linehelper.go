c6ba77a2554f7bef49162ddf6a09151f80856640
yuta-2001 <yu0210ta.sug@gmail.com>
2024-07-07 14:05:11 +0900

    リファクタリング

--- go/linehelper/linehelper.go
+++ go/linehelper/linehelper.go
@@ -1,56 +1,57 @@
 package linehelper
 
 import (
-	"fmt"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
-	"strconv"
-	"io/ioutil"
+    "fmt"
+    "net/http"
+    "net/url"
+    "os"
+    "strings"
+    "strconv"
+    "io/ioutil"
 )
 
-func SendMessage(contributesCount int) {
-	fmt.Println("send no commit notify")
+func SendMessage(contributesCount int) error {
+    fmt.Println("send no commit notify")
 
-	accessToken := os.Getenv("LINE_NOTIFY_TOKEN")
-	fmt.Println(accessToken)
+    accessToken := os.Getenv("LINE_NOTIFY_TOKEN")
+    if accessToken == "" {
+        return fmt.Errorf("LINE_NOTIFY_TOKEN is not set")
+    }
 
-	apiUrl := "https://notify-api.line.me/api/notify"
+    apiUrl := "https://notify-api.line.me/api/notify"
 
-	form := url.Values{}
+    form := url.Values{}
 
-	if contributesCount == 0 {
-		form.Set("message", "草が生えてないよ！やばいよ！")
-		form.Set("stickerPackageId", "6136")
-		form.Set("stickerId", "10551382")
-	} else {
-		form.Set("message", "草が生えてるよ！本日のコミット数は" + strconv.Itoa(contributesCount) + "だよ！")
-		form.Set("stickerPackageId", "446")
-		form.Set("stickerId", "1989")
-	}
+    if contributesCount == 0 {
+        form.Set("message", "草が生えてないよ！やばいよ！")
+        form.Set("stickerPackageId", "6136")
+        form.Set("stickerId", "10551382")
+    } else {
+        form.Set("message", "草が生えてるよ！本日のコミット数は" + strconv.Itoa(contributesCount) + "だよ！")
+        form.Set("stickerPackageId", "446")
+        form.Set("stickerId", "1989")
+    }
 
-	req, err := http.NewRequest("POST", apiUrl, strings.NewReader(form.Encode()))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response Status:", resp.Status)
-	body, err := ioutil.ReadAll(resp.Body)
+    req, err := http.NewRequest("POST", apiUrl, strings.NewReader(form.Encode()))
     if err != nil {
-        fmt.Println("Error reading response body:", err)
-        return
+        return fmt.Errorf("error creating request: %w", err)
+    }
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+    req.Header.Set("Authorization", "Bearer "+accessToken)
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("error sending request: %w", err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("Response Status:", resp.Status)
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return fmt.Errorf("error reading response body: %w", err)
     }
     fmt.Println("Response Body:", string(body))
+
+    return nil
 }
--- go/main.go
+++ go/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"no-commit-notify/go/githubhelper"
-	"no-commit-notify/go/linehelper"
-	"github.com/aws/aws-lambda-go/lambda"
-	"fmt"
+    "no-commit-notify/go/githubhelper"
+    "no-commit-notify/go/linehelper"
+    "github.com/aws/aws-lambda-go/lambda"
+    "fmt"
 )
 
 // HandleRequest is the entry point for AWS Lambda function
 func HandleRequest() {
-	contributesCount := githubhelper.GetContributesCount()
-	linehelper.SendMessage(contributesCount)
-	fmt.Println("Contributes count: ", contributesCount)
-	fmt.Println("Message sent to Line.")
+    contributesCount, err := githubhelper.GetContributesCount()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println("Contributes count:", contributesCount)
+
+    err = linehelper.SendMessage(contributesCount)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println("Message sent successfully")
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+    lambda.Start(HandleRequest)
 }
