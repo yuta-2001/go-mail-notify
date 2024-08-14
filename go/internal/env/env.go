20a759d8ec3751a0610dcd742163b54f4f53d8c6
yuta-2001 <yu0210ta.sug@gmail.com>
2024-08-14 22:56:40 +0900

    kms decryptコード

---
+++ go/internal/env/env.go
@@ -0,0 +1,75 @@
+package env
+
+import (
+    "encoding/base64"
+    "os"
+    "fmt"
+
+    "github.com/aws/aws-sdk-go/service/kms"
+)
+
+func GetEnv(svc *kms.KMS) (string, string, string, error) {
+    env := os.Getenv("ENV")
+    if env == "" {
+        return "", "", "", fmt.Errorf("ENV is empty")
+    }
+
+    userName := os.Getenv("USER_NAME")
+    if userName == "" {
+        return "", "", "", fmt.Errorf("USER_NAME is empty")
+    }
+
+    githubToken := os.Getenv("GITHUB_TOKEN")
+    if githubToken == "" {
+        return "", "", "", fmt.Errorf("GITHUB_TOKEN is empty")
+    }
+
+    lineToken := os.Getenv("LINE_LIFF_TOKEN")
+    if lineToken == "" {
+        return "", "", "", fmt.Errorf("LINE_LIFF_TOKEN is empty")
+    }
+
+    if env != "local" {
+        var err error
+        userName, err = decryptVariable(svc, userName)
+        if err != nil {
+            return "", "", "", err
+        }
+
+        githubToken, err = decryptVariable(svc, githubToken)
+        if err != nil {
+            return "", "", "", err
+        }
+
+        lineToken, err = decryptVariable(svc, lineToken)
+        if err != nil {
+            return "", "", "", err
+        }
+    }
+
+    return userName, githubToken, lineToken, nil
+}
+
+
+func decryptVariable(svc *kms.KMS, variable string) (string, error) {
+    if variable == "" {
+        return "", fmt.Errorf("variable is empty")
+    }
+
+    v, err := base64.StdEncoding.DecodeString(variable)
+    if err != nil {
+        return "", err
+    }
+
+    input := kms.DecryptInput{
+        CiphertextBlob: v,
+    }
+
+    result, err := svc.Decrypt(&input)
+    if err != nil {
+        return "", err
+    }
+
+    return string(result.Plaintext), nil
+}
+
--- go/main.go
+++ go/main.go
@@ -4,23 +4,29 @@ import (
     "fmt"
 
     "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/kms"
 
     "no-commit-notify/go/internal/github"
     "no-commit-notify/go/internal/line"
-	"no-commit-notify/go/internal/utils/env"
+    "no-commit-notify/go/internal/env"
 )
 
 // HandleRequest is the entry point for AWS Lambda function
 func HandleRequest() {
-	env, userName, githubToken, lineToken, err := env.GetEnv()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+    
+    sess, err := session.NewSession()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    svc := kms.New(sess)
 
-	if env == "local" {
-		fmt.Println("Running locally")
-	}
+    userName, githubToken, lineToken, err := env.GetEnv(svc)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 
     contributesCount, err := github.GetContributesCount(userName, githubToken)
     if err != nil {
