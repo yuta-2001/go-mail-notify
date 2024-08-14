dc8f928bd9de95c180803c2ea1c37b8da08c20cf
yuta-2001 <yu0210ta.sug@gmail.com>
2024-08-14 23:27:09 +0900

    terraform initを実行

--- terraform/enviroments/dev/main.tf
+++ terraform/enviroments/dev/main.tf
@@ -19,10 +19,11 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 locals {
   account_id                = data.aws_caller_identity.current.account_id
-  region                    = data.aws_caller_identity.current.region
+  region                    = data.aws_region.current.name
   bucket_name               = "${var.project_prefix}-${local.account_id}"
   function_name             = var.project_prefix
   lambda_iam_role_name      = "${var.project_prefix}-iam-lambda-role"
