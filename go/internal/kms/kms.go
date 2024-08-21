f7214d1b6f402df3baeafbbc1dfac550a8051466
yuta-2001 <yu0210ta.sug@gmail.com>
2024-08-21 15:21:45 +0900

    fix

---
+++ go/internal/kms/kms.go
@@ -0,0 +1,56 @@
+package kms
+
+import (
+    "encoding/base64"
+    "fmt"
+    "strings"
+
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/kms"
+
+    "github.com/aws/aws-sdk-go/aws"
+)
+
+type KmsInstanceStruct struct {
+    Client *kms.KMS
+}
+
+var kmsInstance *KmsInstanceStruct
+
+func GetKmsInstance(env string) (*KmsInstanceStruct, error) {
+    if kmsInstance == nil {
+        sess, err := session.NewSession()
+        if env == "local" {
+            sess, err = session.NewSession(&aws.Config{
+                Endpoint: aws.String("http://host.docker.internal:4566"),
+            })
+        }
+        if err != nil {
+            return nil, err
+        }
+        svc := kms.New(sess)
+        kmsInstance = &KmsInstanceStruct{
+            Client: svc,
+        }
+    }
+
+    return kmsInstance, nil
+}
+
+func (kmsInstance *KmsInstanceStruct)DecryptVariable(variable string) (string, error) {
+    v, err := base64.StdEncoding.DecodeString(variable)
+    if err != nil {
+        return "", fmt.Errorf("failed to decode base64 string: %w", err)
+    }
+
+    input := &kms.DecryptInput{
+        CiphertextBlob: v,
+    }
+
+    result, err := kmsInstance.Client.Decrypt(input)
+    if err != nil {
+        return "", fmt.Errorf("failed to decrypt variable: %w", err)
+    }
+
+    return strings.Replace(string(result.Plaintext), "\n", "", -1), nil
+}
